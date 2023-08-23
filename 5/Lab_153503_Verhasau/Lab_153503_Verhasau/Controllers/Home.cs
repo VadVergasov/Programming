24d7c2b1a66e17f53ef2fd3ebfc41f0b0e0a54b1
Vadzim Verhasau <vadim.vergasov2003@gmail.com>
2023-08-23 23:28:52 +0300

    Done lab 1

---
+++ 5/Lab_153503_Verhasau/Lab_153503_Verhasau/Controllers/Home.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab_153503_Verhasau.Controllers {
+	public class Home : Controller {
+		public IActionResult Index() {
+			return View();
+		}
+	}
+}
---
+++ 5/Lab_153503_Verhasau/Lab_153503_Verhasau/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace Lab_153503_Verhasau.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
